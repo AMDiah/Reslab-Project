f5ac4dfcce92cbda07fa894e68e655ef9786bce8
AMDiah <ainulmardiah316@gmail.com>
2024-08-08 01:00:18 +0700

    menambahkan fungsi sensor

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sensor;
 
 class SensorController extends Controller
 {
-    //
+    public function bacakelembapan()
+    {
+    //baca nilai  tabel sensor dan ambil nilai suhu
+    $sensor = Sensor::select('*')->get();
+    return view('bacasuhu', ['nilaisensor' => $sensor]);
+    }
+    public function bacasuhu()
+    {
+    //baca nilai  tabel sensor dan ambil nilai suhu
+    $sensor = Sensor::select('*')->get();
+    return view('bacakelembapan', ['nilaisensor' => $sensor]);
+    }
+    public function simpansensor()
+    {
+        Sensor::where('id', '1')->update(['suhu' => request()->nilaisuhu, 'kelembapan' => request()->nilaikelembapan]);
+    }
 }
